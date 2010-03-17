4fb1ef2196d9e1fd197ecc518ad354e0698e5ec0
Karl Seguin <kseguin@epocal.com>
2010-03-17 14:48:49 -0400

    added start of web framework

--- Metsys.WebOp/Metsys.WebOp.Mvc/Properties/AssemblyInfo.cs
+++ Metsys.WebOp/Metsys.WebOp.Mvc/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Metsys.WebOp.Runner")]
-[assembly: AssemblyDescription("Command runner for WebOp[timizer]")]
+[assembly: AssemblyTitle("Metsys.WebOp.Mvc")]
+[assembly: AssemblyDescription("Web Framework for WebOp[timizer]")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Karl Seguin")]
-[assembly: AssemblyProduct("Metsys.WebOp.Runner")]
-[assembly: AssemblyCopyright("Copyright Karl Seguin 2010")]
+[assembly: AssemblyProduct("Metsys.WebOp.Mvc")]
+[assembly: AssemblyCopyright("Copyright Karl Seguin  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("add9f830-266d-487c-888d-595373330432")]
+[assembly: Guid("5ee56782-76a8-4cc9-ab03-9c63e5887f71")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Metsys.WebOp/Metsys.WebOp.Mvc/WebOpHttpModule.cs
@@ -0,0 +1,31 @@
+namespace Metsys.WebOp.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Web;
+    
+    public class WebOpHttpModule : IHttpModule
+    {
+        private static readonly List<string> _headersToRemove = new List<string> { "X-AspNet-Version", "X-AspNetMvc-Version", "Etag", "Server",  };
+        private static readonly List<string> _longCacheExtensions = new List<string> { ".js", ".css", ".png", ".jpg",".gif", };
+
+        public void Init(HttpApplication context)
+        {
+            context.EndRequest += OnRequestEnd;
+        }
+        public void Dispose(){}
+
+        private void OnRequestEnd(object sender, EventArgs e)
+        {    
+            var context = HttpContext.Current;
+            _headersToRemove.ForEach(h => context.Response.Headers.Remove(h));
+            var extension = Path.GetExtension(context.Request.Url.AbsolutePath);
+            if (_longCacheExtensions.Contains(extension))
+            {
+                context.Response.CacheControl = "Public";
+                context.Response.Expires = 44000; //slightly over 1 month
+            }
+        }
+    }
+}
